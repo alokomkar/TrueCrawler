6f04096635465c7ee64a93e9f2578d3c4a0c25db
alokomkar <alokomkar.gudikote@gmail.com>
2018-09-25 21:08:51 +0530

    Mocking UI

--- app/src/main/java/com/alokomkar/truecrawller/ui/MainActivityFragment.kt
+++ app/src/main/java/com/alokomkar/truecrawller/ui/MainActivityFragment.kt
@@ -2,10 +2,14 @@ package com.alokomkar.truecrawller.ui
 
 import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.os.Handler
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.alokomkar.truecrawller.R
+import com.alokomkar.truecrawller.data.CharacterRequest
+import com.alokomkar.truecrawller.data.RequestType
 import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
@@ -13,12 +17,28 @@ import kotlinx.android.synthetic.main.fragment_main.*
  */
 class MainActivityFragment : Fragment() {
 
+    private val requestList = ArrayList<CharacterRequest>()
+    private lateinit var requestsRvAdapter: RequestsRvAdapter
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View?
             = inflater.inflate(R.layout.fragment_main, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fab.setOnClickListener {  }
+        fab.setOnClickListener { populateData() }
+        rvContent.apply {
+            layoutManager = LinearLayoutManager( context )
+            requestsRvAdapter = RequestsRvAdapter( requestList )
+            adapter = requestsRvAdapter
+        }
+    }
+
+    private fun populateData() {
+        requestList.add( CharacterRequest(RequestType.TenthCharacter, "", "Detailed Description"))
+        requestList.add( CharacterRequest(RequestType.EveryTenthCharacter, "", "10th : Hi\n20th : Engineer\n30th : Day"))
+        requestList.add( CharacterRequest(RequestType.WordCounter, "", "Hi : 23\nHello : 25\nHow : 54\nYes : 34"))
+        requestsRvAdapter.notifyDataSetChanged()
+        Handler().postDelayed({ requestsRvAdapter.toggleProgress( false ) }, 5000)
     }
 }
---
+++ app/src/main/java/com/alokomkar/truecrawller/ui/RequestsRvAdapter.kt
@@ -0,0 +1,50 @@
+package com.alokomkar.truecrawller.ui
+
+import android.support.v4.widget.ContentLoadingProgressBar
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.alokomkar.truecrawller.R
+import com.alokomkar.truecrawller.data.CharacterRequest
+
+class RequestsRvAdapter( private val requests : ArrayList<CharacterRequest> ) : RecyclerView.Adapter<RequestsRvAdapter.ViewHolder>() {
+
+    private var isProgressShown = true
+
+    override fun onCreateViewHolder( parent : ViewGroup, viewType : Int): ViewHolder
+        = ViewHolder( LayoutInflater.from(parent.context).inflate( R.layout.item_request, null, false) )
+
+
+    override fun getItemCount(): Int
+            = requests.size
+
+    override fun onBindViewHolder( holder : ViewHolder, position : Int )
+            = holder.bindData( requests[position] )
+
+    fun toggleProgress( progressShown : Boolean) {
+        isProgressShown = progressShown
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder( itemView : View) : RecyclerView.ViewHolder( itemView ) {
+
+        private val tvTitle = itemView.findViewById<TextView>( R.id.tvTitle )
+        private val tvDetails = itemView.findViewById<TextView>( R.id.tvDetails )
+        private val pbContent = itemView.findViewById<ContentLoadingProgressBar>( R.id.pbContent )
+
+        fun bindData(characterRequest: CharacterRequest) {
+
+            tvTitle.text = characterRequest.requestType.toString()
+            tvDetails.text = characterRequest.details
+
+            if( isProgressShown )
+                pbContent.show()
+            else
+                pbContent.hide()
+
+        }
+
+    }
+}
